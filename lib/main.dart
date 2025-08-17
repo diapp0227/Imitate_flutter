05385d1e364b2491844d1f63b4e9d42c473b44c7
indianelephant <elephantindian0117@gmail.com>
2025-08-18 01:02:14 +0900

    エントリーポイント・HelloWorldを呼び出せるように修正

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,30 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+import 'HelloWorldRepository.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  final HelloWorldRepository repo = HelloWorldRepository();
+  repo.registerMethodHandler();
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -24,24 +48,6 @@ class MyApp extends StatelessWidget {
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
