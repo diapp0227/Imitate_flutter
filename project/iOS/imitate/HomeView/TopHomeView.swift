a2c1591c1f94771ec3c9520944328c2fad531b3f
indianelephant <elephantindian0117@gmail.com>
2026-01-03 04:25:30 +0900

    【iOS】【TopHome】プラスボタン追加

--- project/iOS/imitate/HomeView/TopHomeView.swift
+++ project/iOS/imitate/HomeView/TopHomeView.swift
@@ -9,10 +9,31 @@ import SwiftUI
 
 struct TopHomeView: View {
     var body: some View {
-        ScrollView {
-            VStack(alignment: .leading, spacing: 8) {
-                BalanceView(type: .income, balance: "200000")
-                BalanceView(type: .expenses, balance: "200000")
+        ZStack {
+            ScrollView {
+                VStack(alignment: .leading, spacing: 8) {
+                    BalanceView(type: .income, balance: "200000")
+                    BalanceView(type: .expenses, balance: "200000")
+                }
+            }
+            
+            VStack {
+              Spacer()
+              HStack {
+                Spacer()
+                Button(action: {
+                    tapPlusButton()
+                }) {
+                  Image(systemName: "plus")
+                    .foregroundColor(.white)
+                    .font(.system(size: 24, weight: .bold))
+                    .frame(width: 56, height: 56)
+                    .background(Color.blue)
+                    .clipShape(Circle())
+                    .shadow(radius: 4)
+                }
+                .padding()
+              }
             }
         }
     }
