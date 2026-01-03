d46cb5afe1e1647d594cbf6a0769cbd7057db5bc
indianelephant <elephantindian0117@gmail.com>
2026-01-03 15:21:36 +0900

    【Android】プラスボタン押下時処理追加・(TopHomeView.kt内にviewModelを作成し、viewModelからRepositoryクラスを呼び出す)

--- project/Android/app/src/main/java/com/example/imitate_android/BaseHome/TopHomeView.kt
+++ project/Android/app/src/main/java/com/example/imitate_android/BaseHome/TopHomeView.kt
@@ -10,14 +10,20 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.imitate_android.BalanceRecordRepository
 import com.example.imitate_android.BaseHome.TopHome.BalanceView
 import com.example.imitate_android.BaseHome.TopHome.BalanceViewType
+import kotlinx.coroutines.launch
+
 
 @Composable
-fun TopHomeView() {
+fun TopHomeView(viewModel: TopHomeViewModel = TopHomeViewModel()) {
 
     Box(
         modifier = Modifier
@@ -37,10 +43,21 @@ fun TopHomeView() {
             )
         }
 
+        // プラスボタン押下時
         FloatingActionButton(
             onClick = {
-
-                
+//                val data: Map<String, Any> = mapOf(
+//                    "type" to "支出",
+//                    "incomeCategory" to "",
+//                    "expenseCategory" to "食費",
+//                    "amount" to 1200,
+//                    "memo" to "昼ごはん",
+//                    "date" to "2026-01-02",
+//                    "createdAt" to "2026-01-02",
+//                    "gameFlag" to false
+//                )
+//                viewModel.insertRecord(data)
+//                viewModel.selectAllData()
             },
             modifier = Modifier
                 .align(Alignment.BottomEnd)
@@ -52,4 +69,41 @@ fun TopHomeView() {
             )
         }
     }
+}
+
+class TopHomeViewModel : ViewModel() {
+
+    private val _uiState = mutableStateOf<TopHomeUiState>(TopHomeUiState.Idle)
+    // TODO: compose側から参照してステータス判定し、表示を出し分ける
+    val uiState = _uiState
+
+    fun selectAllData() {
+        viewModelScope.launch {
+            _uiState.value = TopHomeUiState.Loading
+            try {
+                val data = BalanceRecordRepository.selectAll()
+                _uiState.value = TopHomeUiState.Success(data)
+            } catch (e: Exception) {
+                _uiState.value = TopHomeUiState.Error(e.message ?: "不明なエラー")
+            }
+        }
+    }
+
+    fun insertRecord(data: Map<String, Any>) {
+        viewModelScope.launch {
+            _uiState.value = TopHomeUiState.Loading
+            try {
+                val data = BalanceRecordRepository.insert(data)
+            } catch (e: Exception) {
+
+            }
+        }
+    }
+}
+
+sealed class TopHomeUiState {
+    object Idle : TopHomeUiState()
+    object Loading : TopHomeUiState()
+    data class Success(val data: List<Map<String, Any>>) : TopHomeUiState()
+    data class Error(val message: String) : TopHomeUiState()
 }
